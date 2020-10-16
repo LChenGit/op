e4bcd3e8b9df72ad7ea327ec98eb28a0226c4d70
lchengit <lechen@iastate.edu>
2020-10-15 22:18:23 -0500

    change task 4

--- task4/dot_openmp.c
+++ task4/dot_openmp.c
@@ -35,13 +35,14 @@ int main(int argc, char *argv[])
     /* 
    Perform the dot product in an OpenMP parallel region for loop with a sum reduction 
     */
+    double starttime = omp_get_wtime();
 
 #pragma omp parallel private(i, tid, psum) num_threads(threads)
     {
         psum = 0.0;
         tid = omp_get_thread_num();
-
-#pragma omp for reduction(+: sum)
+#pragma omp for reduction(+ \
+                          : sum)
         for (i = 0; i < len * threads; i++)
         {
             sum += (a[i] * b[i]);
@@ -58,4 +59,5 @@ int main(int argc, char *argv[])
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC; // end time of execution
     printf("Time elpased for parallel program is %f seconds\n", time_spent);
+    printf("calculation took %lf sec\n", omp_get_wtime() - starttime);
 }
